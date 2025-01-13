26abefe2acee73f49ae058405ec622e788550d63
AmeriK88 <lanzaltura17@gmail.com>
2025-01-13 19:05:53 +0000

    Fix: corrección en mostrado del modal llogin cuando hay errores

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm, LoginForm
 from django.contrib import messages
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
+
+csrf_protect
 def register_view(request):
+    show_register_modal = False  
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -18,14 +22,18 @@ def register_view(request):
             return redirect('home')
         else:
             messages.error(request, "Error al registrarse. Por favor revisa los datos ingresados.")
+            show_register_modal = True  
     else:
         form = RegisterForm()
 
-    return render(request, 'core/home.html', {'form': form})
+    return render(request, 'core/home.html', {'form': form, 'show_register_modal': show_register_modal})
+
 
 
 @csrf_protect
 def login_view(request):
+    show_login_modal = False  
+
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
@@ -35,11 +43,11 @@ def login_view(request):
             return redirect('home')
         else:
             messages.error(request, "Usuario o contraseña incorrectos.")
+            show_login_modal = True  
     else:
         form = LoginForm()
 
-    return render(request, 'core/home.html', {'form': form})
-
+    return render(request, 'core/home.html', {'form': form, 'show_login_modal': show_login_modal})
 
 def logout_view(request):
     logout(request)
