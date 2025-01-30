b0df255dc30b705709b6b49d4220554727ddcb87
AmeriK88 <lanzaltura17@gmail.com>
2025-01-30 21:08:36 +0000

    feat: created app special messages - showing offers, special messages or importan warnings for users to see - improved admin panel with admin-interface - created context_processor for special messages and modidied styles z-index for message visibility

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class MensajeEspecial(models.Model):
+    titulo = models.CharField(max_length=200, blank=True, null=True)
+    contenido = models.TextField()
+    activo = models.BooleanField(default=True)
+    fecha_inicio = models.DateField(blank=True, null=True)
+    fecha_fin = models.DateField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.activo:
+            # Desactivar otros mensajes activos
+            MensajeEspecial.objects.filter(activo=True).exclude(id=self.id).update(activo=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo if self.titulo else f"Mensaje {self.id}"
