815fd5c88fef2af019aac6b402c5bb5e04a05795
AmeriK88 <lanzaltura17@gmail.com>
2025-04-12 00:55:31 +0100

    fix: updating utils in appointments app due to duplicate function

--- appointments/utils.py
+++ appointments/utils.py
@@ -3,20 +3,6 @@ from .models import Cita
 from appointments.models import BloqueoFecha
 
 def verificar_disponibilidad(fecha, hora_inicio, duracion_servicio):
-    hora_fin = hora_inicio + timedelta(minutes=duracion_servicio)
-    
-    citas_existentes = Cita.objects.filter(date=fecha)
-
-    for cita in citas_existentes:
-        cita_fin = datetime.combine(cita.date, cita.time) + timedelta(minutes=cita.service.duracion)
-
-        # Comprobar si hay solapamiento
-        if (hora_inicio < cita_fin) and (hora_fin > datetime.combine(cita.date, cita.time)):
-            return False  
-
-    return True  
-
-def verificar_disponibilidad(fecha, hora_inicio, duracion_servicio):
     """
     Verifica si la fecha y hora seleccionadas están disponibles.
     """
